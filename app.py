abada544792c9938e74e694e9ec5de3fa1c0b27a
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-04-29 20:37:16 +0330

    File Sepradted in packages2.

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-from flaskapp import app
+from flaskapp import app, bot_methods
+from config.secret import url
+
 
 if __name__ == "__main__":
+    bot_methods.remove_webhook()
+    bot_methods.set_webhook(url)
     app.run(debug=True)
--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -1,8 +1,7 @@
-from flask import Flask, request, Response
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from TLMethods.Telegram import Telegram
 from config.secret import token, url
-
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '4edc0281f3899c05d40adf12a1102fef'
@@ -10,5 +9,5 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
 bot_methods = Telegram(token)
-bot_methods.remove_webhook()
-bot_methods.set_webhook(url)
+
+from flaskapp import routes  # noqa: E402

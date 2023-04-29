74f6ef41117151d81d59948c5ee7171fa5b5b050
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-04-29 13:11:50 +0330

    logic test for transfer file to server.

--- app.py
+++ app.py
@@ -14,10 +14,14 @@ def index():
     if request.method == 'POST':
         msg = request.get_json()
         chat_id = msg['message']['chat']['id']
-        # txt = msg['message']['text']
-        from_chat_id = msg['message']['from']['id']
-        message_id = msg['message']['message_id']
-        bot_methods.forward_message(message_id, chat_id, from_chat_id)
+        txt = msg['message']['text']
+        if txt == "Hi":
+            bot_methods.send_message("Aleyk Hi", chat_id)
+        else:
+            bot_methods.send_message("What???", chat_id)
+        # from_chat_id = msg['message']['from']['id']
+        # message_id = msg['message']['message_id']
+        # bot_methods.forward_message(message_id, chat_id, from_chat_id)
         return Response('ok', status=200)
     else:
         return '<h1>Not OK</h1>'

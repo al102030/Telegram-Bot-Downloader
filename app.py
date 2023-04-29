9e6a231fb651e89c98c4ede615dc9ab964f078ca
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-04-29 19:39:40 +0330

    Database was created and filled with some fake data.

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
+# from models import User, Download
 from flask import Flask, request, Response
-from config.secret import token, url
+from flask_sqlalchemy import SQLAlchemy
 from TLMethods.Telegram import Telegram
+from config.secret import token, url
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '4edc0281f3899c05d40adf12a1102fef'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
 
 bot_methods = Telegram(token)
-bot_methods.remove_webhook()
-bot_methods.set_webhook(url)
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -25,4 +29,6 @@ def index():
 
 
 if __name__ == "__main__":
+    bot_methods.remove_webhook()
+    bot_methods.set_webhook(url)
     app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from __main__ import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    telegram_id = db.Column(db.Integer, unique=True, nullable=False)
+    username = db.Column(db.String(20), unique=True)
+    mobile = db.Column(db.String(20), unique=True)
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(60), nullable=False)
+    usage = db.Column(db.Integer, default=0)
+    download = db.relationship('Download', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.telegram_id}', '{self.mobile}')"
+
+
+class Download(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    link = db.Column(db.Text, nullable=False)
+    file_wight = db.Column(db.Integer, nullable=False)
+    file_type = db.Column(db.String(20), nullable=False)
+    date_downloaded = db.Column(
+        db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Download('{self.link}', '{self.file_wight}', '{self.file_type}')"

3f1e0dbb827940767f00b853a6c5e2e3f2f78492
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-05-01 10:26:19 +0330

    Update master route to recognize old user or add new user.

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -1,8 +1,6 @@
 from flask import request, Response
-from flaskapp.models import User, Download
-from flaskapp import app, bot_methods, db, bcrypt
-
-registration = 0
+from flaskapp.models import User
+from flaskapp import app, bot_methods, db
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -12,20 +10,28 @@ def index():
         chat_id = msg['message']['chat']['id']
         txt = msg['message']['text']
         user = User.query.filter_by(telegram_id=chat_id)
-        if user:
-            bot_methods.send_message(
-                "You already registered in my user's list, Welcome back!", chat_id)
-        if txt == "/c1":
-            bot_methods.send_message("Your Status: ", chat_id)
-        elif txt == "/c2":
-            bot_methods.send_message("Login: ", chat_id)
-        elif txt == "/c3":
-            bot_methods.send_message("About: ", chat_id)
-        elif txt == "/c4":
-            bot_methods.send_message("Logout: ", chat_id)
-        elif txt == "/c5":
-            registration = 1
-            bot_methods.send_message("Please Choose a Username: ", chat_id)
+        if txt == "/start":
+            if user:
+                bot_methods.send_message(
+                    f"You already registered in my user's list, Welcome back! (Your Telegram ID: {chat_id})", chat_id)
+            else:
+                bot_methods.send_message(
+                    f"You are not registered in my user's list, Welcome! (Your Telegram ID: {chat_id})", chat_id)
+                user = User(telegram_id=chat_id, credit=0)
+                db.session.add(user)
+                db.session.commit()
+        else:
+            pass
+        # if txt == "/c1":
+        #     bot_methods.send_message("Your Status: ", chat_id)
+        # elif txt == "/c2":
+        #     bot_methods.send_message("Login: ", chat_id)
+        # elif txt == "/c3":
+        #     bot_methods.send_message("About: ", chat_id)
+        # elif txt == "/c4":
+        #     bot_methods.send_message("Logout: ", chat_id)
+        # elif txt == "/c5":
+        #     bot_methods.send_message("Please Choose a Username: ", chat_id)
 
         return Response('ok', status=200)
     else:

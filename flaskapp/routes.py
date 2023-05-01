d211fd33bf39f73441ae8b8001f4426bd8fd2f79
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-05-01 23:44:15 +0330

    Add a method to telegram methods to check channel membership.1

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -17,9 +17,9 @@ def index():
                 bot_methods.send_message(
                     f"You already registered in my user's list, Welcome back! (Your Telegram ID: {chat_id})", chat_id)
                 ans = bot_methods.get_chat_member(
-                    channel_id, chat_id)["status"]
-                bot_methods.send_message(str(ans), chat_id)
-                if ans == True:
+                    channel_id, chat_id).get_json()
+                bot_methods.send_message(str(ans["status"]), chat_id)
+                if ans == "member":
                     bot_methods.forward_message(4, chat_id, private_channel_id)
             else:
                 bot_methods.send_message(

4f15a4369e097dd0e78afa319a91bca3fa66079e
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-05-01 11:41:29 +0330

    Change database address in __init_ file

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flaskapp import app, bot_methods
 from config.secret import url
-from flaskapp.models import User
 
 
 if __name__ == "__main__":
-    user = User.query.filter_by(telegram_id="112042461").first()
     bot_methods.remove_webhook()
     bot_methods.set_webhook(url)
     app.run(debug=True)

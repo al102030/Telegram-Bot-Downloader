e53a76ef5ec247c4b14f972a679112556984f0d8
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-05-01 12:38:07 +0330

    Change database address to full pass.

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -11,7 +11,7 @@ def index():
         txt = msg['message']['text']
         user = User.query.filter_by(telegram_id=chat_id).first()
         if txt == "/start":
-            if True:
+            if user:
                 bot_methods.send_message(
                     f"You already registered in my user's list, Welcome back! (Your Telegram ID: {chat_id})", chat_id)
             else:
@@ -21,8 +21,8 @@ def index():
                 db.session.add(user)
                 db.session.commit()
         else:
-            # if txt == "/c1":
-            bot_methods.send_message(txt, chat_id)
+            if txt == "/c1":
+                bot_methods.send_message(txt, chat_id)
             # elif txt == "/c2":
             #     bot_methods.send_message("Login: ", chat_id)
             # elif txt == "/c3":

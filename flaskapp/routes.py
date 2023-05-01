2a69a305822e71f2bda0c9e33a5d741d63020aac
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-05-01 15:31:49 +0330

    Initial status route for check.

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -43,3 +43,4 @@ def status(chat_id):
     if request.method == 'POST':
         user = User.query.filter_by(telegram_id=chat_id).first()
         bot_methods.send_message(f"Your credit is: {user.credit}", chat_id)
+        return redirect(url_for('index'))

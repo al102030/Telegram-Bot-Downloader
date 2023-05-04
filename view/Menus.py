30b5bea0c46827f09d5af2dee93063f97b8e7219
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-05-05 00:41:24 +0330

    Menus and keyboardes seprated to view package.

---
+++ view/Menus.py
@@ -0,0 +1,39 @@
+joining_channel_keyboard = [
+    [{
+        "text": "I already joined!",
+                "callback_data": "A1"
+    }],
+    [{
+        "text": "Join to channel",
+                "url": "https://t.me/al102030_D"
+    }]
+]
+
+credit_charge_keyboard = [
+    [{
+        "text": "5-Gig",
+        "callback_data": "5g"
+    },
+        {
+        "text": "10-Gig",
+        "callback_data": "10g"
+    }],
+    [{
+        "text": "20-Gig",
+        "callback_data": "20g"
+    },
+        {
+        "text": "30-Gig",
+        "callback_data": "30g"
+    }]
+]
+
+
+simple_options = [[
+    {
+        "text": "Yes",
+    },
+    {
+        "text": "No",
+    }]
+]

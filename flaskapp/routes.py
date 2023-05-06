9a052e0b53237e1ca875f03485952b0c9b8cdf77
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-05-06 13:45:29 +0330

    Getting callback data to check

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -14,12 +14,12 @@ def index():
         if "callback_query" in msg:
             callback_from_id = msg['callback_query']['from']['id']
             callback_data = msg['callback_query']['data']
-            if callback_data == "01d0cfb8b904ad4":
+            if callback_data == "01d0cfb8b904ad49":
                 bot_methods.send_message(
                     "🍀Thank you for joining us.🍀\nNow you can use our services.", callback_from_id)
             elif callback_data == "e01fdd230aeaa411":
                 bot_methods.send_message(
-                    "5 Gigabyte add to your account.\ncongratulations!", callback_from_id)
+                    str(msg), callback_from_id)
             elif callback_data == "1a710b5dc955e113":
                 bot_methods.send_message(
                     "10 Gigabyte add to your account.\ncongratulations!", callback_from_id)

9341ab085fc90a1818bbf2e49bef12232921354c
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-05-27 22:19:02 +0330

    Check download telegram videos by telegram methods. load jason

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -149,6 +149,7 @@ def index():
                     #     "https://al102030.pythonanywhere.com/static/DL/"+download.file_name+download.file_type, chat_id)
         elif is_video:
             bot_methods.send_message("Part 1", chat_id)
+            chat_id = msg['message']['chat']['id']
             file_id = msg['message']['video']['file_id']
             # file_size = msg['message']['video']['file_size"']
             bot_methods.send_message("Part 2", chat_id)

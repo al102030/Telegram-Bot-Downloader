e3eed5e0e7b4aab1de8e07e281e0f5ac87864dc3
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-05-27 22:51:31 +0330

    Check download telegram videos by telegram methods. load jason

--- app.py
+++ app.py
@@ -2,11 +2,12 @@ from flaskapp import bot_methods, app
 from config.secret import LINK
 
 if __name__ == "__main__":
-    video_path = bot_methods.get_file(
+    video = bot_methods.get_file(
         "BAACAgQAAxkBAAIHyWRyJjnMClrwfkUTDIaT58r-IJhQAALtDQAC--eJU8APKqhFDi7ILwQ")
-    bot_methods.send_message(video_path, "112042461")
-    # bot_methods.download_file(
-    #     video_path, "okok"+'-telegram')
+    path = video['file_path']
+    # bot_methods.send_message(video_path, "112042461")
+    bot_methods.download_file(
+        path, "okok"+'-telegram')
     # bot_methods.remove_webhook()
     # bot_methods.set_webhook(LINK)
     # app.run(debug=True)

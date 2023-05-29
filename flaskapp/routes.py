02b13e1df544c56ea4837ae5973a0c90f2c3c734
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-05-29 13:01:33 +0330

    Change download location permission by os module.

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -14,7 +14,7 @@ from view.Menus import joining_channel_keyboard, credit_charge_keyboard, simple_
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    os.chmod('static/*', 0o755)
+    os.chmod('static/', 0o755)
     if request.method == 'POST':
         channel_id = "-1001904767094"
         msg = request.get_json()

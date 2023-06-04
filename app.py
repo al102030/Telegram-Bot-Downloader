321a9837c7cb6b8f75ffc04e8f211be8b2633436
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-06-05 00:00:11 +0330

    Change download method for files which have size more than 20 MB by telethon2

--- app.py
+++ app.py
@@ -1,18 +1,8 @@
 from flaskapp import bot_methods, app
-from config.secret import LINK, API_ID, API_HASH
-from telethon import TelegramClient
+from config.secret import LINK
 
 
 if __name__ == "__main__":
-
-    client = TelegramClient("cli-session", API_ID, API_HASH)
-
-    async def main():
-        # Now you can use all client methods listed below, like for example...
-        await client.send_message('me', 'Hello to myself!')
-
-    with client:
-        client.loop.run_until_complete(main())
-    # bot_methods.remove_webhook()
-    # bot_methods.set_webhook(LINK)
-    # app.run(debug=True)
+    bot_methods.remove_webhook()
+    bot_methods.set_webhook(LINK)
+    app.run(debug=True)

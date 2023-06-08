762106f051feaa3b4df16fecb57743d5a08a2228
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-06-08 20:26:18 +0330

    Integrate document and video download method.

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -218,15 +218,15 @@ def index():
             else:
                 if user.credit >= size_mb:
                     try:
-                        # add_new_download('telegram', user.id,
-                        #                  file_name, size_mb)
-                        # run(async_download(bot_methods.download_media(
-                        #     file_name, chat_id), bot_methods.send_chat_action('upload_video', chat_id)))
-                        # update_user_credit(chat_id, size_mb)
-                        # os.chmod(
-                        #     f'/usr/share/nginx/html/static/{file_name}', 0o755)
-                        # bot_methods.send_message(
-                        #     "https://telapi.digi-arya.ir/static/"+file_name, chat_id)
+                        add_new_download('telegram', user.id,
+                                         file_name, size_mb)
+                        run(async_download(bot_methods.download_media(
+                            file_name, chat_id), bot_methods.send_chat_action('upload_video', chat_id)))
+                        update_user_credit(chat_id, size_mb)
+                        os.chmod(
+                            f'/usr/share/nginx/html/static/{file_name}', 0o755)
+                        bot_methods.send_message(
+                            "https://telapi.digi-arya.ir/static/"+file_name, chat_id)
                         bot_methods.send_message(
                             "You can use this direct link for 1 month. Please save your Link.", chat_id)
                         update_download_status(file_name)

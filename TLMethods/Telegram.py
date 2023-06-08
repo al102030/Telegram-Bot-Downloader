b39c396a57125d73360c72eaa1a5fac15fb2a6d2
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-06-08 20:42:07 +0330

    Integrate document and video download method.

--- TLMethods/Telegram.py
+++ TLMethods/Telegram.py
@@ -392,7 +392,7 @@ class Telegram:
             message = await client.get_messages(
                 message.peer_id.user_id, ids=message.id)
             # print(message)
-            print(message.message.update_id)
+            print(message.document)
             # if message:
             #     if "application/" in message.document.mime_type:
             #         print("it is a document or app!")

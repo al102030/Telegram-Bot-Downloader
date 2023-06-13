43d112d70b9e75c331f67d1345340d64c48366db
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-06-13 13:29:26 +0330

    Add restrict_chat_member method to download process.

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -118,7 +118,8 @@ def index():
                     else:
                         bot_methods.send_message(
                             "Enter your YouTube Link to start your download: ", chat_id)
-                        bot_methods.set_chat_permissions(chat_id)
+                        bot_methods.answer_callback_query(
+                            "Please wait a second until your download link send to you.", show_alert=True)
 
                 elif txt == "/c4":
                     options = simple_options

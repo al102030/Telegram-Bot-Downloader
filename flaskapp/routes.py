6d51f8f88e584a4854c136484c6dcb779f576998
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-06-14 12:54:12 +0330

    Add send_async__message to telegram methods.

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -224,10 +224,12 @@ def index():
                         add_new_download('telegram', user.id,
                                          file_name, size_mb)
                         # bot_methods.restrict_chat_member(chat_id)
-                        run(async_download(bot_methods.download_media(
-                            file_name, chat_id, mime_type), bot_methods.send_chat_action(
-                                'upload_video', chat_id), bot_methods.send_async_message(
-                                    "Your download has started!", "Please wait moments for the download link to be sent to you.\n Thanks.", chat_id)))
+                        run(async_download(
+                            bot_methods.download_media(
+                                file_name, chat_id, mime_type),
+                            bot_methods.send_async_message(
+                                "Your download has started!", "Please wait moments for the download link to be sent to you.\n Thanks.", chat_id),
+                            bot_methods.send_chat_action('upload_video', chat_id)))
                         # print(f"Status is: {x}")
                         update_user_credit(chat_id, size_mb)
                         # os.chmod(

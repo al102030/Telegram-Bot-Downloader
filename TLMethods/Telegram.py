9819c4a15f44502d647b0cdbc40d9ff96b82b6e6
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-06-14 15:30:51 +0330

    Download files which doesn't contain media.

--- TLMethods/Telegram.py
+++ TLMethods/Telegram.py
@@ -450,15 +450,15 @@ class Telegram:
                     file_name = file_name+'.mp4'
                     await client.download_media(message.media, file=f'{path}{file_name}')
                     print("Video downloaded!")
-            elif message.file:
+            elif message.message:
                 if "application/" in mime_type:
                     print("it is a document(file) or app!")
-                    await client.download_file(message.file, file=f'{path}{file_name}')
+                    await client.download_file(message.message, file=f'{path}{file_name}')
                     print("Document downloaded!")
                 elif mime_type == "video/mp4":
                     print("it is a video!")
                     file_name = file_name+'.mp4'
-                    await client.download_file(message.file, file=f'{path}{file_name}')
+                    await client.download_file(message.message, file=f'{path}{file_name}')
                     print("Video downloaded!")
                 else:
                     print("File format not supported!")

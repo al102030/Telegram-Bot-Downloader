bdbb0c4a9553592a21e3cc2e644d30339a523389
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-06-15 13:42:46 +0330

    Check pytube regex error in the server.

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -139,69 +139,68 @@ def index():
                                 cookies = pickle.load(file)
                             yt = YouTube(txt)
                             print(yt.video_id)
-                            yt.cookies = cookies
-                            file_name = str(yt.streams.first().default_filename).replace(
-                                " ", "-")[:-5]  # secrets.token_hex(8)
-                            db_methods.add_new_download(
-                                txt, user.id, file_name, 0)
-                            resolution_select_keyboard = []
-                            for stream in (yt.streams.order_by('resolution').desc().filter(adaptive=True, file_extension='mp4')):
-                                lst = []
-                                dictionary = {}
-                                dictionary['text'] = stream.resolution
-                                dictionary['callback_data'] = stream.resolution
-                                lst.append(dictionary)
-                                resolution_select_keyboard.append(lst)
-                            bot_methods.send_message_with_menu("Please select the resolution that you want to download",
-                                                               chat_id, resolution_select_keyboard)
+                            # yt.cookies = cookies
+                            # file_name = str(yt.streams.first().default_filename).replace(
+                            #     " ", "-")[:-5]  # secrets.token_hex(8)
+                            # db_methods.add_new_download(
+                            #     txt, user.id, file_name, 0)
+                            # resolution_select_keyboard = []
+                            # for stream in (yt.streams.order_by('resolution').desc().filter(adaptive=True, file_extension='mp4')):
+                            #     lst = []
+                            #     dictionary = {}
+                            #     dictionary['text'] = stream.resolution
+                            #     dictionary['callback_data'] = stream.resolution
+                            #     lst.append(dictionary)
+                            #     resolution_select_keyboard.append(lst)
+                            # bot_methods.send_message_with_menu("Please select the resolution that you want to download",
+                            #                                    chat_id, resolution_select_keyboard)
                 elif txt == '1080p' or txt == '720p' or txt == '480p' or txt == '360p' or txt == '240p' or txt == '144p':
-                    print(yt.video_id)
-                    # if user.credit > 0:
-                    #     download = Download.query.filter_by(
-                    #         status=0, user_id=user.id).first()
-                    #     if download:
-                    #         user = User.query.filter_by(
-                    #             telegram_id=chat_id).first()
-                    #         with open('cookies.pkl', 'rb') as file:
-                    #             cookies = pickle.load(file)
-                    #         yt = YouTube(download.link)
-                    #         yt.cookies = cookies
-                    #         stream = yt.streams.filter(res=txt).first()
-                    #         if stream:
-                    #             size_mb = stream.filesize / 1000000
-                    #             if size_mb < 0:
-                    #                 size_mb = 1
-                    #             else:
-                    #                 size_mb = round(size_mb)
-                    #             db_methods.update_download_size(
-                    #                 download.file_name, size_mb)
-                    #             if user.credit >= size_mb:
-                    #                 try:
-                    #                     print(size_mb)
-                    #                     stream.download(
-                    #                         output_path='/usr/share/nginx/html/static/', filename=download.file_name+'.mp4')
-                    #                     bot_methods.send_chat_action(
-                    #                         'upload_video', chat_id)
-                    #                     os.chmod(
-                    #                         f'/usr/share/nginx/html/static/{download.file_name}.mp4', 0o755)
-                    #                     bot_methods.send_message(
-                    #                         "https://telapi.digi-arya.ir/static/"+download.file_name+".mp4", chat_id)
-                    #                     db_methods.update_user_credit(
-                    #                         chat_id, size_mb)
-                    #                     db_methods.update_download_status(
-                    #                         download.file_name)
-                    #                 except ValueError as error:
-                    #                     print(
-                    #                         'Caught this error: ' + repr(error))
-                    #             else:
-                    #                 inline_keyboard = credit_charge_keyboard
-                    #                 bot_methods.send_message_with_keyboard(
-                    #                     "You don't have enough account credit to begin the download.\nPlease select one of the options below to debit your account.\nThank you",
-                    #                     chat_id, inline_keyboard)
-                    #         else:
-                    #             bot_methods.send_message(
-                    #                 "File doesn't Exist.\nPlease try another Resolution or link\nThanks.",
-                    #                 chat_id)
+                    if user.credit > 0:
+                        download = Download.query.filter_by(
+                            status=0, user_id=user.id).first()
+                        if download:
+                            user = User.query.filter_by(
+                                telegram_id=chat_id).first()
+                            with open('cookies.pkl', 'rb') as file:
+                                cookies = pickle.load(file)
+                            yt = YouTube(download.link)
+                            yt.cookies = cookies
+                            stream = yt.streams.filter(res=txt).first()
+                            if stream:
+                                size_mb = stream.filesize / 1000000
+                                if size_mb < 0:
+                                    size_mb = 1
+                                else:
+                                    size_mb = round(size_mb)
+                                db_methods.update_download_size(
+                                    download.file_name, size_mb)
+                                if user.credit >= size_mb:
+                                    try:
+                                        print(size_mb)
+                                        stream.download(
+                                            output_path='/usr/share/nginx/html/static/', filename=download.file_name+'.mp4')
+                                        bot_methods.send_chat_action(
+                                            'upload_video', chat_id)
+                                        os.chmod(
+                                            f'/usr/share/nginx/html/static/{download.file_name}.mp4', 0o755)
+                                        bot_methods.send_message(
+                                            "https://telapi.digi-arya.ir/static/"+download.file_name+".mp4", chat_id)
+                                        db_methods.update_user_credit(
+                                            chat_id, size_mb)
+                                        db_methods.update_download_status(
+                                            download.file_name)
+                                    except ValueError as error:
+                                        print(
+                                            'Caught this error: ' + repr(error))
+                                else:
+                                    inline_keyboard = credit_charge_keyboard
+                                    bot_methods.send_message_with_keyboard(
+                                        "You don't have enough account credit to begin the download.\nPlease select one of the options below to debit your account.\nThank you",
+                                        chat_id, inline_keyboard)
+                            else:
+                                bot_methods.send_message(
+                                    "File doesn't Exist.\nPlease try another Resolution or link\nThanks.",
+                                    chat_id)
 
                 else:
                     bot_methods.send_message(

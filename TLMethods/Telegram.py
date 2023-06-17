c6c70177f542f16044ddd4bc9dde4815a1b56a6c
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-06-17 13:29:14 +0330

    Get message from other users have problem.

--- TLMethods/Telegram.py
+++ TLMethods/Telegram.py
@@ -446,18 +446,17 @@ class Telegram:
                 else:
                     print("Media format not supported!")
             elif message.message:
-                print("iiinjaaaaaaaaa")
-                # if "application/" in mime_type:
-                #     print("it is a document(file) or app!")
-                #     await client.download_media(message.message, file=file)
-                #     print("Document downloaded!(message)")
-                # elif mime_type == "video/mp4":
-                #     print("it is a video!")
-                #     file += '.mp4'
-                #     await client.download_file(message.message, file=file)
-                #     print("Video downloaded!(message)")
-                # else:
-                #     print("File format not supported!")
+                if "application/" in mime_type:
+                    print("it is a document(file) or app!")
+                    await client.download_media(message.message, file=file)
+                    print("Document downloaded!(message)")
+                elif mime_type == "video/mp4":
+                    print("it is a video!")
+                    file += '.mp4'
+                    await client.download_file(message.message, file=file)
+                    print("Video downloaded!(message)")
+                else:
+                    print("File format not supported!")
             else:
                 print("The message doesn't contain media.")
 

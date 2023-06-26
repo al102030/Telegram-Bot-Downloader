2d85696119eaf0b3604cad3e52f9a9990fbaf71e
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-06-26 19:28:55 +0330

    Telegram api download_media method cand download a 300 mb file.

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -16,8 +16,8 @@ from view.Menus import joining_channel_keyboard, credit_charge_keyboard, simple_
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == 'POST':
-        # print("Got it!")
-        # return Response('ok', status=200)
+        print("Got it!")
+        return Response('ok', status=200)
         channel_id = "-1001904767094"
         msg = request.get_json()
         db_methods = Methods()

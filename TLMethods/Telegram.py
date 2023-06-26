1626ead4ce5948d3101e6c8dd146c401ab7d49ce
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-06-26 07:53:06 +0330

    Telegram api download_media method cand download a 300 mb file.

--- TLMethods/Telegram.py
+++ TLMethods/Telegram.py
@@ -391,24 +391,12 @@ class Telegram:
                 message = item
                 message_id = message.id
                 break
-            chunk_size = 10 * 1024 * 1024  # 10 MB
             file = path+file_name
             if message.media:
                 if "application/" in mime_type:
-                    result = client.download_media(
-                        message.media,
-                        file=file,
-                        progress_callback=lambda current, total: print(
-                            f'{current}/{total}'),
-                        # part_size=chunk_size,
-                        iter_chunk_size=chunk_size,
-                        skip_big_files=True,
-                        wait_time=5,
-                        timeout=60
-                    )
-                    # print("it is a document(media) or app!")
-                    # await client.loop.run_until_complete(client.download_media(message.media, file=file))
-                    # print("Document downloaded!(media)")
+                    print("it is a document(media) or app!")
+                    await client.loop.run_until_complete(client.download_media(message.media, file=file, progress_callback=lambda current, total: print(f'{current}/{total}')))
+                    print("Document downloaded!(media)")
                 elif mime_type == "video/mp4":
                     print("it is a video!")
                     file += '.mp4'

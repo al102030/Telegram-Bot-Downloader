521c4bf8af60ec1f20b4dbee7b21da2017cdd350
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-07-12 20:42:06 +0330

    Telegram api download_media method leave downloads uncomplete.

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ async def async_download(func1, func2):
 
 if __name__ == "__main__":
     run(async_download(bot_methods.send_async_message(
-        "Your download has started!\nPlease wait.", "112042461"), bot_methods.download_media()))
+        "Your download has started!\nPlease wait.", "112042461"), bot_methods.download_media("okok")))
 
     # bot_methods.remove_webhook()
     # bot_methods.set_webhook(LINK)
-    # app.run(debug=True)
+    app.run(debug=True, port=8000)
